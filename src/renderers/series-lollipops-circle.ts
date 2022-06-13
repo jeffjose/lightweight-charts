123e1722ff7a08f10badc57bee72e0373c214e89
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-06-12 21:19:25 -0700

    hittest working

--- src/renderers/series-lollipops-circle.ts
+++ src/renderers/series-lollipops-circle.ts
@@ -112,18 +112,18 @@ export function hitTestCircle(
 	const circleSize = shapeSize('circle', item.size);
 	const circleSizeNonPixelRatio = circleSize / pixelRatio;
 
+	const pos = item.position === 'top' ? getPosForPositionTop(item, circleSize, strokeWidth) : getPosForPositionBottom(item, circleSize, strokeWidth);
+
 	// We need to scale everything by pixelRatio because of the quirkiness
 	// in draw() we scale everything by pixelRatio. Here absolute numbers in draw() like circleSize, strokeRadius needs to be scaled down
-	const centerX = item.x;
-	const centerY = item.y + (circleSizeNonPixelRatio - 1) / 2 + strokeWidthNonPixelRatio;
 
 	// Radius
-	const tolerance = (circleSizeNonPixelRatio - 1) / 2 + strokeWidthNonPixelRatio;
+	const radius = (circleSizeNonPixelRatio - 1) / 2 + strokeWidthNonPixelRatio;
 
-	const xOffset = centerX - x;
-	const yOffset = centerY - y;
+	const xOffset = (pos.centerX / pixelRatio) - x;
+	const yOffset = (pos.centerY / pixelRatio) - y;
 
 	const dist = Math.sqrt(xOffset * xOffset + yOffset * yOffset);
 
-	return dist <= tolerance;
+	return dist <= radius;
 }
