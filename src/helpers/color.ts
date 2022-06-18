9c170e0f65a8de45a19140050333f7ea501023bc
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-06-17 21:18:54 -0700

    adding a comment and moving the creation of gradient down for optimization.

--- src/helpers/color.ts
+++ src/helpers/color.ts
@@ -352,10 +352,12 @@ export function interpolateColorValueAt(color1: string, color2: string, offset: 
 
 // eslint-disable-next-line max-params
 export function getCanvasGradientsFrom2Colors(ctx: CanvasRenderingContext2D, color1: string, color2: string, x0: number, y0: number, x1: number, y1: number): CanvasRenderingContext2D['strokeStyle'] | CanvasRenderingContext2D['fillStyle'] {
-	const gradient = ctx.createLinearGradient(x0, y0, x1, y1);
-	if (deltaE(color1, color2) < 2) {
+	// <=1.0 is not perceptible by human eyes
+	// http://zschuessler.github.io/DeltaE/learn/
+	if (deltaE(color1, color2) < 1) {
 		return color1;
 	}
+	const gradient = ctx.createLinearGradient(x0, y0, x1, y1);
 	const totalStops = 3;
 	for (const i of Array.from(Array(totalStops).keys()).map((x: number) => x + 1)) {
 		gradient.addColorStop(i / totalStops, interpolateColorValueAt(color1, color2, i / totalStops));
